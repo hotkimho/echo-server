a5958451345555d34df9289a69d296696126da3f
kimho <rlaghekd777@naver.com>
2023-03-05 20:58:20 +0900

    # WIP - gin 테스트

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("김남갱 멍청 멍청")
+	r := gin.Default()
+	r.GET("/heartbeat", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "success",
+		})
+	})
+
+	r.Run("127.0.0.1:8080")
 }
